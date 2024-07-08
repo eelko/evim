edee51aa467b06ee3beefabf22190f3c40e916db
Eelko <{ID}+{username}@users.noreply.github.com>
2024-07-08 07:08:46 +0200

    Updates from lazy

--- lua/eelko/core/keymaps.lua
+++ lua/eelko/core/keymaps.lua
@@ -16,6 +16,7 @@ keymap.set("n", "<leader>wv", "<C-w>v", { desc = "Split window vertically" }) --
 keymap.set("n", "<leader>wh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>we", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>wx", "<cmd>close<CR>", { desc = "Close current window" }) -- close current split window
+keymap.set("n", "<leader>c", "<cmd>close<CR>", { desc = "Close current window" }) -- close current split window
 
 -- keymap.set("n", "<leader>t", "", { desc = "Tabs" }) 
 -- keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
@@ -28,9 +29,129 @@ keymap.set("n", "<leader>p", "", { desc = "Plugins" })
 keymap.set("n", "<leader>pl", "<cmd>Lazy<CR>", { desc = "Lazy" })
 keymap.set("n", "<leader>pm", "<cmd>Mason<CR>", { desc = "Mason" })
 
--- Buffer management
-keymap.set("n", "]b", "<cmd>bnext<CR>", { desc = "Next buffer" }) 
-keymap.set("n", "[b", "<cmd>bprev<CR>", { desc = "Previous buffer" }) 
+-- better up/down
+keymap.set({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+keymap.set({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+keymap.set({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
+keymap.set({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
+
+-- Move to window using the <ctrl> hjkl keys
+keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
+
+-- Resize window using <ctrl> arrow keys
+keymap.set("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+keymap.set("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+
+-- Move Lines
+keymap.set("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move Down" })
+keymap.set("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move Up" })
+keymap.set("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+keymap.set("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+keymap.set("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move Down" })
+keymap.set("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move Up" })
+
+-- buffers
 keymap.set("n", "<leader>b", "", { desc = "Buffers" })
+keymap.set("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+keymap.set("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+keymap.set("n", "[b", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+keymap.set("n", "]b", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+-- keymap.set("n", "<leader>`", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
+keymap.set("n", "<leader>bD", "<cmd>:bd<cr>", { desc = "Delete Buffer and Window" })
+-- keymap.set("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
 keymap.set("n", "<leader>bb", "<cmd>Telescope buffers<cr>", { desc = "Telescope Buffers" })
 keymap.set("n", "<leader>bc", "<cmd>%bd<CR><cmd>e#<CR><cmd>bd#<CR>", { desc = "Close all buffers but this one" })
+
+-- Clear search with <esc>
+keymap.set({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and Clear hlsearch" })
+
+
+-- Clear search, diff update and redraw
+-- taken from runtime/lua/_editor.lua
+keymap.set(
+  "n",
+  "<leader>ur",
+  "<Cmd>nohlsearch<Bar>diffupdate<Bar>normal! <C-L><CR>",
+  { desc = "Redraw / Clear hlsearch / Diff Update" }
+)
+
+-- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
+keymap.set("n", "n", "'Nn'[v:searchforward].'zv'", { expr = true, desc = "Next Search Result" })
+keymap.set("x", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next Search Result" })
+keymap.set("o", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next Search Result" })
+keymap.set("n", "N", "'nN'[v:searchforward].'zv'", { expr = true, desc = "Prev Search Result" })
+keymap.set("x", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev Search Result" })
+keymap.set("o", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev Search Result" })
+
+-- Add undo break-points
+keymap.set("i", ",", ",<c-g>u")
+keymap.set("i", ".", ".<c-g>u")
+keymap.set("i", ";", ";<c-g>u")
+
+-- save file
+keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
+
+--keywordprg
+keymap.set("n", "<leader>K", "<cmd>norm! K<cr>", { desc = "Keywordprg" })
+
+-- better indenting
+keymap.set("v", "<", "<gv")
+keymap.set("v", ">", ">gv")
+
+
+-- new file
+keymap.set("n", "<leader>n", "<cmd>enew<cr>", { desc = "New File" })
+
+keymap.set("n", "<leader>xl", "<cmd>lopen<cr>", { desc = "Location List" })
+keymap.set("n", "<leader>xq", "<cmd>copen<cr>", { desc = "Quickfix List" })
+
+keymap.set("n", "[q", vim.cmd.cprev, { desc = "Previous Quickfix" })
+keymap.set("n", "]q", vim.cmd.cnext, { desc = "Next Quickfix" })
+
+-- formatting
+keymap.set({ "n", "v" }, "<leader>lf", function()
+  LazyVim.format({ force = true })
+end, { desc = "Format" })
+
+-- diagnostic
+local diagnostic_goto = function(next, severity)
+  local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
+  severity = severity and vim.diagnostic.severity[severity] or nil
+  return function()
+    go({ severity = severity })
+  end
+end
+keymap.set("n", "<leader>ld", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
+keymap.set("n", "]d", diagnostic_goto(true), { desc = "Next Diagnostic" })
+keymap.set("n", "[d", diagnostic_goto(false), { desc = "Prev Diagnostic" })
+keymap.set("n", "]e", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
+keymap.set("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
+keymap.set("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
+keymap.set("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
+
+-- stylua: ignore start
+
+-- toggle options
+keymap.set("n", "<leader>uT", function() if vim.b.ts_highlight then vim.treesitter.stop() else vim.treesitter.start() end end, { desc = "Toggle Treesitter Highlight" })
+
+-- quit
+keymap.set("n", "<leader>q", "", { desc = "Quit" })
+keymap.set("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit All" })
+
+-- highlights under cursor
+-- keymap.set("n", "<leader>ui", vim.show_pos, { desc = "Inspect Pos" })
+-- keymap.set("n", "<leader>uI", "<cmd>InspectTree<cr>", { desc = "Inspect Tree" })
+
+ 
+keymap.set("n", "<leader>ww", "<C-W>p", { desc = "Other Window", remap = true })
+keymap.set("n", "<leader>wd", "<C-W>c", { desc = "Delete Window", remap = true })
+keymap.set("n", "<leader>w\\", "<C-W>s", { desc = "Split Window Below", remap = true })
+keymap.set("n", "<leader>w|", "<C-W>v", { desc = "Split Window Right", remap = true })
+keymap.set("n", "<leader>\\", "<C-W>s", { desc = "Split Window Below", remap = true })
+keymap.set("n", "<leader>|", "<C-W>v", { desc = "Split Window Right", remap = true })
+
--- lua/eelko/plugins/nvim-tree.lua
+++ lua/eelko/plugins/nvim-tree.lua
@@ -15,28 +15,41 @@ return {
         relativenumber = true,
       },
       -- change folder arrow icons
-      renderer = {
-        indent_markers = {
-          enable = true,
-        },
-        icons = {
-          glyphs = {
-            git = {
-              unstaged = "U",
-              staged = "S",
-              unmerged = "UM",
-              renamed = "R",
-              deleted = "D",
-              untracked = "",
-              ignored = "I",
-            },
-            folder = {
-              arrow_closed = "", -- arrow when folder is closed
-              arrow_open = "", -- arrow when folder is open
-            }, 
-          },
-        },
-      },
+      -- renderer = {
+      --   indent_markers = {
+      --     enable = true,
+      --   },
+      --   icons = {
+      --     show = {
+      --       file = true,
+      --       folder = true,
+      --       folder_arrow = true,
+      --       git = true,
+      --     },
+      --     glyphs = {
+      --       default = "",
+      --       symlink = "",
+      --       git = {
+      --         unstaged = "U",
+      --         staged = "S",
+      --         unmerged = "UM",
+      --         renamed = "R",
+      --         deleted = "D",
+      --         untracked = "",
+      --         ignored = "I",
+      --       },
+      --       folder = {
+      --         -- arrow_closed = "", -- arrow when folder is closed
+      --         -- arrow_open = "", -- arrow when folder is open
+      --         default = "",
+      --         open = "",
+      --         empty = "",
+      --         empty_open = "",
+      --         symlink = "",
+      --       },
+      --     },
+      --   },
+      -- },
       -- disable window_picker for
       -- explorer to work well with
       -- window splits
@@ -64,5 +77,50 @@ return {
     keymap.set("n", "<leader>e", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "File-tree" }) -- toggle file explorer on current file
     -- keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
     -- keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
-  end
+  end,
+  opts = {
+    sources = { "filesystem", "buffers", "git_status" },
+    open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
+    filesystem = {
+      bind_to_cwd = false,
+      follow_current_file = { enabled = true },
+      use_libuv_file_watcher = true,
+    },
+    window = {
+      mappings = {
+        ["l"] = "open",
+        ["h"] = "close_node",
+        ["<space>"] = "none",
+        ["Y"] = {
+          function(state)
+            local node = state.tree:get_node()
+            local path = node:get_id()
+            vim.fn.setreg("+", path, "c")
+          end,
+          desc = "Copy Path to Clipboard",
+        },
+        ["O"] = {
+          function(state)
+            require("lazy.util").open(state.tree:get_node().path, { system = true })
+          end,
+          desc = "Open with System Application",
+        },
+        ["P"] = { "toggle_preview", config = { use_float = false } },
+      },
+    },
+    default_component_configs = {
+      indent = {
+        with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+        expander_collapsed = "",
+        expander_expanded = "",
+        expander_highlight = "NeoTreeExpander",
+      },
+      git_status = {
+        symbols = {
+          unstaged = "󰄱",
+          staged = "󰱒",
+        },
+      },
+    },
+  }
 }
